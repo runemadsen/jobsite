cd4a5b5b958676862bb70dc326a8b11129f4638c
Rune Madsen <rune@runemadsen.com>
2014-10-18 17:11:03 -0400

    more

--- spec/controllers/api/logins_spec.rb
+++ spec/controllers/api/logins_spec.rb
@@ -4,15 +4,24 @@ describe "api/logins" do
   
   describe "POST" do
 
+    def expect_one_user_with_login
+      expect(User.count).to eq(1)
+      expect(Login.count).to eq(1)
+      expect(User.first.email).to eq("rune@runemadsen.com")
+      expect(Login.first.user.email).to eq("rune@runemadsen.com")
+    end
+
+    def expect_no_user_or_login
+      expect(User.count).to eq(0)
+      expect(Login.count).to eq(0)
+    end
+
     context "if new email" do
       it "creates user and sends email" do
         expect(Pony).to receive(:deliver).once
         post '/api/logins', :email => "rune@runemadsen.com"
         expect(last_response.status).to eq(201)
-        expect(User.count).to eq(1)
-        expect(User.first.email).to eq("rune@runemadsen.com")
-        expect(Login.count).to eq(1)
-        expect(Login.first.user.email).to eq("rune@runemadsen.com")
+        expect_one_user_with_login
       end
     end
 
@@ -22,7 +31,7 @@ describe "api/logins" do
         expect(Pony).to receive(:deliver).once
         post '/api/logins', :email => "rune@runemadsen.com"
         expect(last_response.status).to eq(200)
-        expect(User.count).to eq(1)
+        expect_one_user_with_login
       end
     end
 
@@ -31,7 +40,7 @@ describe "api/logins" do
         expect(Pony).to receive(:deliver).never
         post '/api/logins', :email => "Rune Madsen <rune@runemadsen.com>"
         expect(last_response.status).to eq(400)
-        expect(User.count).to eq(0)
+        expect_no_user_or_login
         expect(last_json["message"]).to eq(Madsen::WrongArgument.new(:email).to_s)
       end
     end
@@ -41,7 +50,7 @@ describe "api/logins" do
         expect(Pony).to receive(:deliver).never
         post '/api/logins'
         expect(last_response.status).to eq(422)
-        expect(User.count).to eq(0)
+        expect_no_user_or_login
         expect(last_json["message"]).to eq(Madsen::MissingArgument.new(:email).to_s)
       end
     end
